ff23e9dbc2979d17c7394688a7bc04823fee2802
e63062kp <konrad.painta@student.manchester.ac.uk>
2024-03-03 13:39:54 +0000

    Fixed random shuffle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { Tile, TileRow, Mistakes } from './components';
 import './styles.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleQuestion } from '@fortawesome/free-regular-svg-icons';
 
 function App() {
 
@@ -14,6 +16,7 @@ function App() {
   ]);
   const [numberOfSelected, setNumberOfSelected] = useState(0);
   const [check, setCheck] = useState(false);
+  const [gameStart , setGameStart] = useState(false);
 
   const [connectionsMade, setConnectionsMade] = useState(Array(4).fill(null));
 
@@ -51,8 +54,12 @@ function App() {
   function shuffle() {
     let newItems = items.slice();
     let newSelections = selected.slice();
-    for (let i = newItems.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
+
+    const min = connectionsMade.findIndex((element) => element === null) * 4;
+    const max = 16;
+
+    for (let i = max - 1; i > min; i--) {
+      const j = Math.floor(Math.random() * (max - min) + (min));
       [newItems[i], newItems[j]] = [newItems[j], newItems[i]];
       [newSelections[i], newSelections[j]] = [newSelections[j], newSelections[i]];
     }
@@ -105,71 +112,93 @@ function App() {
 
   }
 
+  useEffect(() => {
+
+    shuffle();
+  }, []);
+
   return (
-    <div className='container'>
-    <p>Create four groups of four!</p>
-    <div className='grid'>
-      {connectionsMade[0] ? (
-        <div className='row'>
-        <TileRow item={connectionsMade[0]} mapping={connections} difficulty={0}/>
-        </div>
+    <div>
+      {gameStart ? (
+            <div className='container'>
+            <p>Create four groups of four!</p>
+            <div className='grid'>
+              {connectionsMade[0] ? (
+                <div className='row'>
+                <TileRow item={connectionsMade[0]} mapping={connections} difficulty={0}/>
+                </div>
+              ) : (
+                <div className='row'>
+                {
+                  [0, 1, 2, 3].map((index) => (
+                    <Tile item={items[index]} select={selected[index]} onSelect={() => handleClick(index)}/>
+                  ))
+                }
+              </div>
+              )}
+              {connectionsMade[1] ? (
+                <div className='row'>
+                <TileRow item={connectionsMade[1]} mapping={connections} difficulty={1}/>
+                </div>
+              ) : (
+                <div className='row'>
+                {
+                  [4, 5, 6, 7].map((index) => (
+                    <Tile item={items[index]} select={selected[index]} onSelect={() => handleClick(index)}/>
+                  ))
+                }
+              </div>
+              )}
+              {connectionsMade[2] ? (
+                <div className='row'>
+                <TileRow item={connectionsMade[2]} mapping={connections} difficulty={2}/>
+                </div>
+              ) : (
+                <div className='row'>
+                {
+                  [8, 9, 10, 11].map((index) => (
+                    <Tile item={items[index]} select={selected[index]} onSelect={() => handleClick(index)}/>
+                  ))
+                }
+              </div>
+              )}
+              {connectionsMade[3] ? (
+                <div className='row'>
+                <TileRow item={connectionsMade[3]} mapping={connections} difficulty={3}/>
+                </div>
+              ) : (
+                <div className='row'>
+                {
+                  [12, 13, 14, 15].map((index) => (
+                    <Tile item={items[index]} select={selected[index]} onSelect={() => handleClick(index)}/>
+                  ))
+                }
+              </div>
+              )}
+            </div>
+            <Mistakes />
+            <div className='button-container'>
+              <button className='RoundButton' onClick={resetSelections}>Reset</button>
+              <button className='RoundButton'onClick={shuffle}>Shuffle</button>
+              <button className='RoundButton' onClick={checkSelection} disabled={numberOfSelected!==4}>Check</button>
+              <button 
+                style={{ background: 'transparent', border: 'none', cursor: 'pointer' }} 
+                onClick={() => setGameStart(false)}
+              >
+                <FontAwesomeIcon icon={faCircleQuestion} size='xl'/>
+              </button>
+            </div>
+          </div>
       ) : (
-        <div className='row'>
-        {
-          [0, 1, 2, 3].map((index) => (
-            <Tile item={items[index]} select={selected[index]} onSelect={() => handleClick(index)}/>
-          ))
-        }
-      </div>
-      )}
-      {connectionsMade[1] ? (
-        <div className='row'>
-        <TileRow item={connectionsMade[1]} mapping={connections} difficulty={1}/>
+        <div className='container'>
+          <div className='panel'>
+            <h1>Connections</h1>
+            <p>Try to make four groups of four!</p>
+            <button className='RoundButton' onClick={() => setGameStart(true)}>Start</button>
+          </div>
         </div>
-      ) : (
-        <div className='row'>
-        {
-          [4, 5, 6, 7].map((index) => (
-            <Tile item={items[index]} select={selected[index]} onSelect={() => handleClick(index)}/>
-          ))
-        }
-      </div>
-      )}
-      {connectionsMade[2] ? (
-        <div className='row'>
-        <TileRow item={connectionsMade[2]} mapping={connections} difficulty={2}/>
-        </div>
-      ) : (
-        <div className='row'>
-        {
-          [8, 9, 10, 11].map((index) => (
-            <Tile item={items[index]} select={selected[index]} onSelect={() => handleClick(index)}/>
-          ))
-        }
-      </div>
-      )}
-      {connectionsMade[3] ? (
-        <div className='row'>
-        <TileRow item={connectionsMade[3]} mapping={connections} difficulty={3}/>
-        </div>
-      ) : (
-        <div className='row'>
-        {
-          [12, 13, 14, 15].map((index) => (
-            <Tile item={items[index]} select={selected[index]} onSelect={() => handleClick(index)}/>
-          ))
-        }
-      </div>
       )}
     </div>
-    <Mistakes />
-    <div className='button-container'>
-    <button className='RoundButton' onClick={resetSelections}>Reset</button>
-    <button className='RoundButton'onClick={shuffle}>Shuffle</button>
-    <button className='RoundButton' onClick={checkSelection} disabled={numberOfSelected!==4}>Check</button>
-      
-    </div>
-  </div>
   );
 }
 
